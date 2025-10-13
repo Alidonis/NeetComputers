feaf100a68830170499f092f4a40a9867a5b4951
RedToast <alexanderdostal71@gmail.com>
2025-10-13 10:05:21 -0500

    fixed spelling mistake

--- src/main/java/com/redtoast/Connections/PeripheralReceiver.java
+++ src/main/java/com/redtoast/Connections/PeripheralReceiver.java
@@ -5,7 +5,7 @@ import com.redtoast.simulation.value.Value;
 import java.util.List;
 import java.util.UUID;
 
-public interface PeripheralReciver {
+public interface PeripheralReceiver {
     List<PeripheralProvider> scanForPeripherals();
     Value<?> sendFunctionCall(UUID uuid, String functionName, Value<?>... args);
 }
