9b6fd82081c04b7abe6df8b0ce5483f298fafc9e
RedToast <alexanderdostal71@gmail.com>
2025-11-23 22:20:10 -0600

    removed the temporary modem block

--- src/main/java/com/redtoast/blocks/modem/ModemBlock.java
+++
@@ -1,9 +0,0 @@
-package com.redtoast.blocks.modem;
-
-import net.minecraft.block.Block;
-
-public class ModemBlock extends Block {
-    public ModemBlock(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/com/redtoast/blocks/modem/ModemBlockEntity.java
+++
@@ -1,4 +0,0 @@
-package com.redtoast.blocks.modem;
-
-public class ModemBlockEntity {
-}
