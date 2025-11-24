8e82e32414ff212ad43659cdd70b554ae0a54d6b
RedToast <alexanderdostal71@gmail.com>
2025-11-23 22:21:35 -0600

    added the office computer

---
+++ src/main/java/com/redtoast/blocks/OfficeComputer/OfficeBlockComputer.java
@@ -0,0 +1,39 @@
+package com.redtoast.blocks.OfficeComputer;
+
+import com.redtoast.blocks.generic.ComputerBlock;
+import com.redtoast.neet.BulkRegistery;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class OfficeBlockComputer extends ComputerBlock {
+    public OfficeBlockComputer(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public BlockEntityType<? extends BlockEntity> getType() {
+        return BulkRegistery.fetchBlockEntityType("office_computer");
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context){
+        return VoxelShapes.cuboid(1f / 16f, 0f, 2f / 16f, 15f / 16f, 13f / 16f, 15f / 16f);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new OfficeEntityComputer(pos, state);
+    }
+}
---
+++ src/main/java/com/redtoast/blocks/OfficeComputer/OfficeEntityComputer.java
@@ -0,0 +1,18 @@
+package com.redtoast.blocks.OfficeComputer;
+
+import com.redtoast.blocks.generic.ComputerBlockEntity;
+import com.redtoast.computerSpecs;
+import com.redtoast.neet.BulkRegistery;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+
+public class OfficeEntityComputer extends ComputerBlockEntity{
+    public OfficeEntityComputer(BlockPos pos, BlockState state) {
+        super(BulkRegistery.fetchBlockEntityType("office_computer"), pos, state, new computerSpecs()
+            .setBinaryGraphicsSize(11, 6)
+            .setColorGraphicsSize(384,288)
+            .setIPS(60000)
+            .setMachineName("Office Computer")
+        );
+    }
+}
