51db93111bf47ec11e7c9dd99257cd13269f0bcb
RedToast <alexanderdostal71@gmail.com>
2026-04-04 18:56:03 -0500

    removed debug statements

--- src/main/java/com/redtoast/neet/config/BooleanConfigOption.java
+++ src/main/java/com/redtoast/neet/config/BooleanConfigOption.java
@@ -8,7 +8,6 @@ public class BooleanConfigOption extends ConfigOption<Boolean>{
     @Override
     Boolean parseFromString(String string) {
         string = string.trim();
-        System.out.println(string);
         if (string.equalsIgnoreCase("true")) return true;
         if (string.equalsIgnoreCase("false")) return false;
         throw new RuntimeException();
