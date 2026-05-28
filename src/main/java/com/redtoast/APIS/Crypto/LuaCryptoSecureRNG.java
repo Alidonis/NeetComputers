c7709fb1c636959326b2f4374456b04cd1aa175c
Alidonis <astunkawikidot@gmail.com>
2026-05-28 21:18:09 +0200

    Fixed an overflow condition within SecureRNG, changed type to double to account for the highest-value case in lua runtime (which is also the default)

--- src/main/java/com/redtoast/APIS/Crypto/LuaCryptoSecureRNG.java
+++ src/main/java/com/redtoast/APIS/Crypto/LuaCryptoSecureRNG.java
@@ -7,19 +7,23 @@ import java.security.SecureRandom;
 
 public class LuaCryptoSecureRNG implements Exposable {
     @Exposed
-    public int GetRandomInt() {
-        return new SecureRandom().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    public double GetRandomInt() {
+        return new SecureRandom().nextDouble(Double.MIN_VALUE, Double.MAX_VALUE);
     }
     @Exposed
-    public int GetRandomIntFromMin(int min) {
-        return new SecureRandom().nextInt(min, Integer.MAX_VALUE);
+    public double GetRandomIntFromMin(double min) {
+        return new SecureRandom().nextDouble(min, Double.MAX_VALUE);
     }
     @Exposed
-    public int GetRandomIntUpTo(int max) {
-        return new SecureRandom().nextInt(Integer.MIN_VALUE, max);
+    public double GetRandomIntUpTo(double max) {
+        if (max == Double.MAX_VALUE)
+            return new SecureRandom().nextDouble(Double.MIN_VALUE, max);
+        return new SecureRandom().nextDouble(Double.MIN_VALUE, max + 1);
     }
     @Exposed
-    public int GetRandomBetween(int min, int max) {
-        return new SecureRandom().nextInt(min, max + 1);
+    public double GetRandomBetween(double min, double max) {
+        if (max == Double.MAX_VALUE)
+            return new SecureRandom().nextDouble(min, max);
+        return new SecureRandom().nextDouble(min, max + 1);
     }
 }
