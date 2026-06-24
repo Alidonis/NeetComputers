591f1b6d8683a8b2d6f6c6fe259c04c86a5d368e
RedToast <alexanderdostal71@gmail.com>
2026-06-24 17:32:08 -0500

    made filesystem template variable

--- src/main/java/com/redtoast/items/generics/DiskItem.java
+++ src/main/java/com/redtoast/items/generics/DiskItem.java
@@ -4,11 +4,13 @@ import com.redtoast.simulation.FS.DiskError;
 import com.redtoast.simulation.FS.DiskSystem;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 import java.util.UUID;
 
 public interface DiskItem {
-    int getAddress(ItemStack stack, BlockEntity caller);
+    int getAddress(ItemStack stack, World world);
     DiskSystem generateSystem(ItemStack stack, UUID uuid, BlockEntity caller) throws DiskError;
     void markBootable(ItemStack stack, boolean state, BlockEntity caller);
+    boolean isBootable(ItemStack stack, World world);
 }
