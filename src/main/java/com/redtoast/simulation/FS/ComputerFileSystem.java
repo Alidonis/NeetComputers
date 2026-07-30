fd633995182224d03a14c49853fb3d5157d70ff5
RedToast <alexanderdostal71@gmail.com>
2026-07-30 18:50:34 -0500

    added better error logs for computers failing to load due to language incompatibilities

--- src/main/java/com/redtoast/simulation/FS/ComputerFileSystem.java
+++ src/main/java/com/redtoast/simulation/FS/ComputerFileSystem.java
@@ -1,5 +1,7 @@
 package com.redtoast.simulation.FS;
 
+import com.redtoast.neet.BinaryLoader;
+
 import java.util.UUID;
 
 public class ComputerFileSystem extends DiskManager{
@@ -8,6 +10,11 @@ public class ComputerFileSystem extends DiskManager{
     public ComputerFileSystem(int homePointer, String template, UUID computerID) throws DiskError {
         super();
         homedisk = createDisk(homePointer, template, computerID);
+        if (homedisk instanceof DiskSystem diskSystem && diskSystem.getBuild().language==null) {
+            if (diskSystem.getBuild().isLua && BinaryLoader.loaded){
+                throw new DiskError("Lua failed to load (check logs, it might be possible to download functionality for your machine)");
+            }else throw new DiskError("Language does not exist");
+        }
         if (!homedisk.isBootable()) throw new DiskError("System not bootable");
     }
 
