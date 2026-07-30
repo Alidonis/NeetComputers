6a81cf634dd22bf720b86ad5946c6412d96f7861
RedToast <alexanderdostal71@gmail.com>
2026-07-30 17:06:51 -0500

    Patched bogus error log produced by running the game without CC installed while the config is not generated

--- src/main/java/com/redtoast/Compat/GetCC.java
+++ src/main/java/com/redtoast/Compat/GetCC.java
@@ -2,17 +2,16 @@ package com.redtoast.Compat;
 
 import com.redtoast.Computer;
 import com.redtoast.Connections.PeripheralProvider;
+import com.redtoast.simulation.APILoader;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GetCC {
     public static PeripheralProvider getPeripheral(BlockPos pos, World world, Computer computer){
         try {
             return computer == null ? CCT.searchForCCT(pos, world) : CCT.searchForCCT(pos, world, computer);
         } catch (Throwable t) {
-            t.printStackTrace();
+            if (!(t instanceof NoClassDefFoundError)) APILoader.printJavaError(t);
             return null;
         }
     }
