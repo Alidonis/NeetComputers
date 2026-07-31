0ae6e57f66c3fa98bc085f82c857df17b6fd0ecd
RedToast <alexanderdostal71@gmail.com>
2026-07-31 16:33:08 -0500

    undid bad tuning

--- src/main/java/com/redtoast/simulation/config/DefaultComputerConfig.java
+++ src/main/java/com/redtoast/simulation/config/DefaultComputerConfig.java
@@ -42,11 +42,11 @@ public class DefaultComputerConfig implements ComputerConfig{
 
     @Override
     public int instructionsPerBatch() {
-        return 10125000;
+        return 3000;
     }
 
     @Override
     public int batchesPerTick() {
-        return 10;
+        return 3750;
     }
 }
